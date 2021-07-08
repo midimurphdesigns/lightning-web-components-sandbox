d98ee12dbdb3b8884ac1fa84c386287add4dfcd5
Kevin Murphy <kevinmurphywebdev@gmail.com>
2021-07-08 09:43:05 -0700

    added more logic to sandbox components

--- force-app/main/default/lwc/conditionalRenderingExample/conditionalRenderingExample.js
+++ force-app/main/default/lwc/conditionalRenderingExample/conditionalRenderingExample.js
@@ -2,4 +2,10 @@ import { LightningElement, track } from "lwc";
 
 export default class ConditionalRenderingExample extends LightningElement {
   @track displayDiv = false;
+
+  cityList = ["Jaipur", "bangluru", "Hyderabad", "Mumbai"];
+
+  showDivHandler(event) {
+    this.displayDiv = event.target.checked;
+  }
 }
